5a16d64a16ebfcd8f8ee2a216718b05f3f3b1b93
Eme <bramasca@gmail.com>
2020-12-06 21:13:57 -0300

    share folder and style

--- index.js
+++ index.js
@@ -9,17 +9,55 @@ morgan('tiny')
 
 app.set('view engine', 'ejs');
 
-app.get('/',(req,res) => {
-	res.render('index', {
-		file_1: 'Poema Guarani',
-	});
+
+app.get('/', (req,res) => {
+	res.status(200).render('language', {})
 })
 
-app.get('/download',(req,res) => {
+app.get('/home/:lang',(req,res) => {
+	const lang = req.params.lang
+	let content;
+	switch (lang) {
+		case 'es' :
+			content = {
+				title: "Biblioteca - Termux",
+				col1: "Nombre del archivo",
+				col2: "Descarga"
+			}
+			break;
+		case 'en' :
+			content = {
+				title: "Library - Termux",
+				col1: "File name",
+				col2: "Download"
+			}
+			break;
+	}
+
+	fs.promises.readdir(path.join(__dirname,'share/'))
+		.then(data => {
+			const files = []	
+			data.forEach(fileName => {
+				files.push({
+					name: fileName,
+					link: "/download/" + data.indexOf(fileName)
+				})
+			})
+			res.status(200).render('index', { files, content })
+		}).catch(err => {
+			res.status(404).send(err)
+		})
+});
+
+
+app.get('/download/:num',(req,res) => {
+	const num = req.params.num	
 	fs.promises.readdir(path.join(__dirname,'share/'))
 	.then(data => {
-		const file = path.join(__dirname,'./share/'+data[0])
-		res.download(file)
+		const file = path.join(__dirname,'./share/'+data[num])
+		res.status(200).download(file)
+	}).catch(err => {
+		res.status(404).send(err)
 	})
 })
 
