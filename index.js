17821cbee27d543d97a2e50947da65c855c54254
Eme <bramasca@gmail.com>
2020-12-06 22:37:31 -0300

    lighthouse improvements

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
 const port = 3000
 const path = require('path')
 const fs = require('fs')
+var os = require( 'os' );
+var opn = require('opn');
 
-morgan('tiny')
+var networkInterfaces = os.networkInterfaces();
+let myIP;
+for ( let net in networkInterfaces ) {
+	if (typeof networkInterfaces[net] !== 'object' ) return;
+	for (let data  in networkInterfaces[net]) {
+		if (networkInterfaces[net][data].family === 'IPv4') {
+			if (myIP === undefined) myIP = networkInterfaces[net][data].address;
+		}
+	}
+}
 
 app.set('view engine', 'ejs');
-
 
 app.get('/', (req,res) => {
 	res.status(200).render('language', {})
@@ -21,6 +30,7 @@ app.get('/home/:lang',(req,res) => {
 		case 'es' :
 			content = {
 				title: "Biblioteca - Termux",
+				share: "Compartir IP por Whatsapp",
 				col1: "Nombre del archivo",
 				col2: "Descarga"
 			}
@@ -28,6 +38,7 @@ app.get('/home/:lang',(req,res) => {
 		case 'en' :
 			content = {
 				title: "Library - Termux",
+				share: "Share IP with Whatsapp",
 				col1: "File name",
 				col2: "Download"
 			}
@@ -43,7 +54,7 @@ app.get('/home/:lang',(req,res) => {
 					link: "/download/" + data.indexOf(fileName)
 				})
 			})
-			res.status(200).render('index', { files, content })
+			res.status(200).render('index', { files, content, myIP })
 		}).catch(err => {
 			res.status(404).send(err)
 		})
@@ -61,7 +72,14 @@ app.get('/download/:num',(req,res) => {
 	})
 })
 
+app.set('trust proxy',true); 
+
+app.get("/ip", (req, res)=>{
+   	res.status(200).send(req.ip); 
+}); 
+
 app.listen(port, () => {
-	console.log(`listening http://localhost:${port}`)
+	console.log(`Opening your Library at http://${myIP}:${port}`)
+	opn(`http://${myIP}:${port}`);
 })
 
